13077e0d130b1b90ab74d2850014c4213ebc6f8a
Erek Röös <erek.roeoes@coduct.com>
2020-07-22 21:35:55 +0200

    fix: do not crash if no favorites are saved

--- src/listFavsHandler.js
+++ src/listFavsHandler.js
@@ -2,6 +2,14 @@ const fs = require("fs-extra");
 const path = require("path");
 
 module.exports = async () => {
-  const favs = await fs.readJson(path.join(process.cwd(), "favs.json"));
+  const favs = await fs.readJson(path.join(process.cwd(), "favs.json"), {
+    throws: false,
+  });
+
+  if (!favs) {
+    console.log("Currently no favorites saved");
+    return;
+  }
+
   console.table(favs);
 };
